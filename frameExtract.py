90333b6ea09b778259222abc1ec9562d952e32f8
THENG CHIA CHIA <127763561+jiaChia02@users.noreply.github.com>
2023-09-12 02:01:35 +0800

    Update frameExtract.py

--- frameExtract.py
+++ frameExtract.py
@@ -1,111 +1,38 @@
 import cv2
-import numpy as np
 import os
-import sys
-from datetime import date
-import datetime
 
-today = str(date.today())
+# Parameters
+frame_folder = 'yolov7\inference\detectImg\\frameGallery65\\result'  # Folder containing your frames (images)
+output_path = 'yolov7\inference\detectImg\\frameGallery65\\result\\result2.mp4'  # Output video path
+frame_rate = 5  # Desired frame rate for the output video (adjust as needed)
+resize_factor = 0.7  # Resize factor (e.g., 0.7 for 30% smaller)
+# Get the list of frames in the folder and sort them based on numerical order in filenames
+frame_files = sorted([os.path.join(frame_folder, f) for f in os.listdir(frame_folder) if f.startswith('frame') and f.endswith('.jpg')], key=lambda x: int(''.join(filter(str.isdigit, x))))
 
-## Directory Manage
-os.chdir("yolov7") # direct to the yolov7 folder
+# Check if any frames were found
+if not frame_files:
+    print("No frames found in the specified folder.")
+    exit()
 
-current_path = os.path.dirname(os.path.abspath(__file__)) # ..\AiLibrary\yolov7
-parent_path = os.path.dirname(current_path) # ..\AiLibrary
-output_path = parent_path + "\Result\\"+ today
-sys.path.append(current_path)
-# import recordData
+# Get the dimensions of the first frame to determine the video size
+first_frame = cv2.imread(frame_files[0])
+height, width, layers = first_frame.shape
 
-# ######To create csv file
-# csvName = 'record.csv'
-# fieldnames = ['Date', 'Time','Folder','frameNo', 'rightHand', 'leftHand', 'bookCoordinate', 'Action']
-#         #Folder: Output1
-#         #Date: 8-9-2023
-#         #Time:
-#         #frameNo: frame1
-#         #rightHand:(0.00, 0.00),(0.00,0.00),(0.00,0.00),(0.00,0.00)
-#         #leftHand:(0.00, 0.00),(0.00,0.00),(0.00,0.00),(0.00,0.00)
-#         #bookCoordinate:(0.00, 0.00),(0.00,0.00),(0.00,0.00),(0.00,0.00)
-#         #Action: [Reading, holding, pending]
-# recordData.csvCreate(output_path,csvName, fieldnames)
+# Calculate the new dimensions for resizing
+new_width = int(width * resize_factor)
+new_height = int(height * resize_factor)
 
-# def chkFolder():
-#     filename = 'output'
-#     folder = recordData.chkAndCreateFolder(output_path, filename)
-#     frameFolder = folder + '\\frameCapture\\'
-#     try:
-#         if not os.path.exists(frameFolder):
-#             print(frameFolder)
-#             os.makedirs(frameFolder)
-            
-#     except OSError:
-#         print("Error: Creating Directory of Data")
-        
-#     return frameFolder 
-    
-# def writeCSV(file_path, date, time, folderName, frame):   
-#     data = [date, time, folderName, frame, ' ', ' ', ' ', ' ']
-    
-#     recordData.csvCreate(file_path, csvName, data)
+# Define the codec and create a VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # You can change the codec as needed (e.g., 'XVID', 'MJPG')
+out = cv2.VideoWriter(output_path, fourcc, frame_rate, (new_width, new_height))
 
-def frameExtract(input, output):
-    print("Start extract frames from ", input)
-    # Open the video file
-    cap = cv2.VideoCapture(input)
+# Iterate through the frames, resize them, and write to the video
+for frame_file in frame_files:
+    frame = cv2.imread(frame_file)
+    resized_frame = cv2.resize(frame, (new_width, new_height))
+    out.write(resized_frame)
 
-    # Get the original video's frame rate (FPS)
-    original_fps = int(cap.get(cv2.CAP_PROP_FPS))
+# Release the VideoWriter
+out.release()
 
-    # Calculate the desired time interval between frames (0.25 seconds)
-    desired_time_interval = 0.25  # in seconds = 0.25
-
-    # Calculate the frame interval based on the desired time interval
-    frame_interval = int(original_fps * desired_time_interval)
-
-    # Initialize a frame counter
-    frame_counter = 1
-
-    while True:
-        ret, frame = cap.read()
-
-        if not ret:
-            break
-
-        # Save the frame if it's the appropriate frame according to the frame interval
-        if frame_counter % frame_interval == 0:
-            # Apply noise reduction
-            frame = cv2.fastNlMeansDenoisingColored(frame, None, 10, 10, 7, 21)
-
-            # Increase contrast and brightness
-            alpha = 1.5  # Contrast control
-            beta = 30    # Brightness control
-            frame = cv2.convertScaleAbs(frame, alpha=alpha, beta=beta)
-            
-            # Get the current time
-            current_time = datetime.datetime.now().time()
-            # Format the time as hh:mm:ss
-            formatted_time = str(current_time.strftime("%H:%M:%S"))
-            # Construct the output frame filename with the "frameX.jpg" format
-            num = frame_counter // frame_interval
-            frameNo = 'frame'+ str(num)
-            #save data to csv file
-            #filepath = os.path.dirname(os.path.dirname(os.path.dirname(output))) #C:\Users\X\AiLibrary\Result\2023-09-08
-            foldername = os.path.basename(os.path.dirname(os.path.dirname(output))) #outputX
-            print("output path:", output_path)
-            
-            #writeCSV(output_path,today, formatted_time, str(foldername), frameNo)
-            
-            output_frame_path = os.path.join(output, f'{frameNo}.jpg')
-            cv2.imwrite(output_frame_path, frame)
-
-        frame_counter += 1
-
-    # Release the video capture object
-    cap.release()
-
-    # Print a message when done
-    print(f"Frames extracted at {desired_time_interval} seconds interval to {output}.")
-
-input_path = current_path + "\inference\\videos\library.mp4"
-output_folder = output_path
-frameExtract(input_path, output_folder)
+print(f"Video saved to {output_path}")
